aff18bf093e37bc448eca63efaa0cbc7c983d6a2
qinshenxue <qinshenxue@qq.com>
2017-08-18 15:42:19 +0800

    ui 组件

--- src/components/comment/index.vue
+++ src/components/comment/index.vue
@@ -8,7 +8,9 @@
         <ul class="v-comment_list">
             <li class="v-comment_list-item" v-for="comment in data">
                 <div class="v-comment_head">
-                    <div class="v-comment_avatar" :style="getAvatar(comment)">{{comment.avatar?'':comment.nick_name.substr(0, 1)}}</div>
+                    <div class="v-comment_avatar" :style="getAvatar(comment)">
+                        {{comment.avatar ? '' : comment.nick_name.substr(0, 1)}}
+                    </div>
                     <div class="v-comment_author">
                         <span class="v-comment_name">{{comment.nick_name}}</span>
                         <span v-if="comment.reply_to"> 回复
@@ -21,7 +23,8 @@
                 </div>
                 <div class="v-comment_foot">
                     <a href="javascript:;" @click="reply=comment">回复</a>
-                    <v-comment-form enable-cancel v-if="reply && comment.id==reply.id" @cancel="reply=null" @submit="submitComment" :placeholder="placeholder" :enable-submit="enableSubmit">
+                    <v-comment-form enable-cancel v-if="reply && comment.id==reply.id" @cancel="reply=null"
+                                    @submit="submitComment" :placeholder="placeholder" :enable-submit="enableSubmit">
                         <template slot="form-foot">
                             <slot name="form-foot"></slot>
                         </template>
@@ -33,68 +36,68 @@
 </template>
 
 <script>
-import vCommentForm from './form.vue';
-export default {
-    name: 'v-comment',
-    props: {
-        enableSubmit: {
-            type: Boolean,
-            default: true
+    import vCommentForm from './form.vue';
+    export default {
+        name: 'v-comment',
+        props: {
+            enableSubmit: {
+                type: Boolean,
+                default: true
+            },
+            data: Array,
+            placeholder: String
         },
-        data: Array,
-        placeholder: String
-    },
-    data() {
-        return {
-            reply: null,
-            colors: {}
-        }
-    },
-    components: {
-        vCommentForm
-    },
-    methods: {
-        colorHexToRGB(color) {
-            color = color.toUpperCase();
-            var regexpHex = /^#[0-9a-fA-F]{3,6}$/;//Hex
-            if (regexpHex.test(color)) {
-                var hexArray = [];
-                var count = 1;
-                for (var i = 1; i <= 3; i++) {
-                    if (color.length - 2 * i > 3 - i) {
-                        hexArray.push(Number("0x" + color.substring(count, count + 2)));
-                        count += 2;
-                    } else {
-                        hexArray.push(Number("0x" + color.charAt(count) + color.charAt(count)));
-                        count += 1;
+        data() {
+            return {
+                reply: null,
+                colors: {}
+            }
+        },
+        components: {
+            vCommentForm
+        },
+        methods: {
+            colorHexToRGB(color) {
+                color = color.toUpperCase();
+                var regexpHex = /^#[0-9a-fA-F]{3,6}$/;//Hex
+                if (regexpHex.test(color)) {
+                    var hexArray = [];
+                    var count = 1;
+                    for (var i = 1; i <= 3; i++) {
+                        if (color.length - 2 * i > 3 - i) {
+                            hexArray.push(Number("0x" + color.substring(count, count + 2)));
+                            count += 2;
+                        } else {
+                            hexArray.push(Number("0x" + color.charAt(count) + color.charAt(count)));
+                            count += 1;
+                        }
+                    }
+                    return "rgba(" + hexArray.join(",") + ",.7)";
+                } else {
+                    return color;
+                }
+            },
+            getAvatar(comment) {
+                if (comment.avatar) {
+                    return {
+                        backgroundColor: 'transparent',
+                        backgroundImage: `url("${comment.avatar}")`
                     }
                 }
-                return "rgb(" + hexArray.join(",") + ",.5)";
-            } else {
-                return color;
-            }
-        },
-        getAvatar(comment) {
-            if (comment.avatar) {
-                return {
-                    backgroundColor: 'transparent',
-                    backgroundImage: `url("${comment.avatar}")`
+                if (!this.colors[comment.nick_name]) {
+                    this.colors[comment.nick_name] = this.colorHexToRGB('#' + Math.floor(Math.random() * 16777215).toString(16));
                 }
+                return {
+                    backgroundColor: this.colors[comment.nick_name]
+                }
+            },
+            submitComment(content, commentForm) {
+                var formData = {content: content};
+                if (this.reply) {
+                    formData.reply_to = this.reply.nick_name;
+                }
+                this.$emit('submit', Object.assign({}, this.reply, formData), commentForm, this);
             }
-            if (!this.colors[comment.nick_name]) {
-                this.colors[comment.nick_name] = this.colorHexToRGB('#' + Math.floor(Math.random() * 16777215).toString(16));
-            }
-            return {
-                backgroundColor: this.colors[comment.nick_name]
-            }
-        },
-        submitComment(content, commentForm) {
-            var formData = { content: content };
-            if (this.reply) {
-                formData.reply_to = this.reply.nick_name;
-            }
-            this.$emit('submit', Object.assign({}, this.reply, formData), commentForm, this);
         }
     }
-}
 </script>
--- src/components/spinner/index.vue
+++ src/components/spinner/index.vue
@@ -1,21 +1,11 @@
 <template>
     <svg class="v-spinner" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120">
-        <defs>
-            <line id="l" x1="60" x2="60" y1="7" y2="27" :stroke="color" stroke-width="11" stroke-linecap="round"/>
-        </defs>
         <g>
-            <use xlink:href="#l" opacity=".27"/>
-            <use xlink:href="#l" opacity=".27" transform="rotate(30 60,60)"/>
-            <use xlink:href="#l" opacity=".27" transform="rotate(60 60,60)"/>
-            <use xlink:href="#l" opacity=".27" transform="rotate(90 60,60)"/>
-            <use xlink:href="#l" opacity=".27" transform="rotate(120 60,60)"/>
-            <use xlink:href="#l" opacity=".27" transform="rotate(150 60,60)"/>
-            <use xlink:href="#l" opacity=".37" transform="rotate(180 60,60)"/>
-            <use xlink:href="#l" opacity=".46" transform="rotate(210 60,60)"/>
-            <use xlink:href="#l" opacity=".56" transform="rotate(240 60,60)"/>
-            <use xlink:href="#l" opacity=".66" transform="rotate(270 60,60)"/>
-            <use xlink:href="#l" opacity=".75" transform="rotate(300 60,60)"/>
-            <use xlink:href="#l" opacity=".85" transform="rotate(330 60,60)"/>
+            <line v-for="i in c" x1="60" x2="60" y1="7" y2="27"
+                  :stroke="color"
+                  stroke-width="11" stroke-linecap="round"
+                  :opacity="i.o"
+                  :transform="i.r?'rotate('+i.r+' 60,60)':null" />
         </g>
     </svg>
 </template>
@@ -26,6 +16,23 @@
             color: {
                 type: String,
                 default: '#000'
+            }
+        },
+        data(){
+            return {
+                c: [{o: 0.27},
+                    {o: 0.27, r: 30},
+                    {o: 0.27, r: 60},
+                    {o: 0.27, r: 90},
+                    {o: 0.27, r: 120},
+                    {o: 0.27, r: 150},
+                    {o: 0.37, r: 180},
+                    {o: 0.46, r: 210},
+                    {o: 0.56, r: 240},
+                    {o: 0.66, r: 270},
+                    {o: 0.75, r: 300},
+                    {o: 0.85, r: 330}
+                ]
             }
         }
     }
